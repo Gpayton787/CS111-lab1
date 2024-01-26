cca540ccbc4398e9589234d192a5b8e26778cdcb
Gpayton787 <107384112+Gpayton787@users.noreply.github.com>
2024-01-26 14:40:46 -0800

    Testing fork(), waitpif(), and execlp()

--- pipe.c
+++ pipe.c
@@ -6,10 +6,29 @@
 
 int main(int argc, char *argv[])
 {
-	// TODO: it's all yours
-	printf("%i\n", argc);
+	// First print args
+	printf("argc: %i\n", argc);
 	for(int i = 0; i < argc; i++){
-		printf("%s\n", argv[i]);
+		printf("arg %i: %s\n", argv[i]);
+	}
+	//Test fork
+	int return_code = fork();
+	if(return_code == 0){
+		//Child
+		printf("Child Process: \n");
+		execlp("ls", "ls", "NULL");
+		exit(0);
+	}
+	else if(return_code > 0){
+		printf("Parent Process: \n");
+		printf("Wait for the child to finish\n");
+		int pid = return_code;
+		int status = 0;
+		waitpid(pid, &status, 0);
+		printf("Child process done, exited with code: %d\n", WEXITSTATUS(status));
+	}
+	else{
+		printf("Child process creation error\n");
 	}
 	
 	return 0;
