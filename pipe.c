73be34eace2d1fa46a4b1d3c19f10fbe0256b8b4
Gpayton787 <107384112+Gpayton787@users.noreply.github.com>
2024-01-26 12:13:05 -0800

    First Commit

---
+++ pipe.c
@@ -0,0 +1,13 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+	// TODO: it's all yours
+	printf("%i\n", argc);
+	
+	return 0;
+}
