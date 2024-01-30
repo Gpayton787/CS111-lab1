cf80300e0821217001daae5062afc51fe8c921ee
Gpayton787 <107384112+Gpayton787@users.noreply.github.com>
2024-01-29 16:03:58 -0800

    Test if child is correctly writing to pipe

--- pipe.c
+++ pipe.c
@@ -22,7 +22,8 @@ int main(int argc, char *argv[])
 		printf("Redirct and run program A\n");
 		dup2(fds[1], STDOUT_FILENO);
 		close(fds[1]);
-		// execlp("ls", "ls", NULL);
+		execlp("ls", "ls", NULL);
+		//break here
 		printf("yoyoyo");
 		exit(0);
 	}
@@ -34,7 +35,7 @@ int main(int argc, char *argv[])
 		printf("Child process done, exited with code: %d\n", WEXITSTATUS(status));
 		printf("Now change stdin to be read end of pipe and run program B\n");
 		char buf[100];
-		read(fds[0], buf, 6);
+		read(fds[0], buf, 100);
 		printf("%s\n", buf);
 		// dup2(fds[0], STDIN_FILENO);
 		// close(fds[0]);
