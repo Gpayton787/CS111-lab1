054326bd1a4dda4484bd0b34c32268e13323dedb
Gpayton787 <107384112+Gpayton787@users.noreply.github.com>
2024-01-30 12:51:47 -0800

    Testing two program with the for loop

--- pipe.c
+++ pipe.c
@@ -12,73 +12,53 @@ int main(int argc, char *argv[])
 	}
 	//If we only have 1 program
 	if(argc == 2){
-		printf("execute %s\n", argv[1]);
-		// execlp(argv[1], argv[1], NULL);
+		execlp(argv[1], argv[1], NULL);
 	}
 	for(int i = 1; i < argc-1; i++){
-		//Create pipes
+		//Create pipe 1
+		printf("Iteration: %i\n", i);
 		int fds1[2];
-		int fds2[2];
 		pipe(fds1);
-		pipe(fds2);
 		//If there are more than 2 processes left
 		if(argc-i > 2){
 			;
 		}
-		//Fork the process
-		int return_code = fork();
-		if(return_code == 0){
-			//Program A
-			printf("Child Process %d:\n", i);
-			//If not the first process read from pipe 2
-			if(i != 1){
-				dup2(fds2[0], STDIN_FILENO);
-				close(fds2[0]);
-				close(fds2[1]);
-				printf("reading from stdin...");
+		else{
+			//There are only two processes left, pipe2 not needed
+			int return_code = fork();
+			if(return_code == 0){
+				//Child
+				
+				//If this NOT the first process, take from read end of pipe2
+				// if(i != 1){
+				// 	dup2(fds2[0], STDIN_FILENO);
+				// 	close(fds2[0]);
+				// 	close(fds2[1]);
+				// 	printf("reading from stdin...");
+				// }
+				//redirect its stdout to write end of pipe 1
+				dup2(fds1[1], STDOUT_FILENO);
+				close(fds1[1]);
+				close(fds1[0]);
+				//Call program
+				execlp(argv[i], argv[i], NULL);
 			}
-			else{
-				printf("not reading from stdin...");
-			}
-			//redirect its stdout to write end of pipe 1
-			dup2(fds1[1], STDOUT_FILENO);
-			//Close pipe ends to prevent hanging
-			close(fds1[1]);
-			close(fds1[0]);
-			//Call program
-			printf("results of program %d\n", i);
-			// execlp(argv[i], argv[i], NULL);
-		}
-		else if(return_code > 0){
-			printf("Parent Process %d:\n", i);
-			int pid = return_code;
-			int status = 0;
-			waitpid(pid, &status, 0);
-			//Take input from read end of pipe
-			dup2(fds1[0], STDIN_FILENO);
-			close(fds1[0]);
-			close(fds1[1]);
-			char buffer[4096];
-    		read(STDIN_FILENO, buffer, 23);
-			//If we are not the last program, redirect to write end of pipe 2
-			if(i+1 != argc-1){
-				dup2(fds2[1], STDOUT_FILENO);
-				//Close pipe ends to prevent hanging
-				close(fds2[1]);
-				close(fds2[0]);
-			}
-			//Else we are the last program
-			printf("%s", buffer);
-			printf("+ program %i\n", i+1);
-			// execlp(argv[i+1], argv[i+1], NULL);
-		}
-	}
-	//If there is a program left over
-	if(argc % 2){
-		//Execute the last program reading from pipe 2
-		;
-	}
+			else if(return_code > 0){
+				//Parent
+				int pid = return_code;
+				int status = 0;
+				waitpid(pid, &status, 0);
+				//Read from pipe 1
+				dup2(fds1[0], STDIN_FILENO);
+				close(fds1[0]);
+				close(fds1[1]);
+				//If we are not the last program, create pipe2 and write to it
 
+				//Else we are the last program
+				execlp(argv[i+1], argv[i+1], NULL);
+			}
+		}
+	}
 	return 0;
 
 	//Static 2 argument program
