d9d191976a7cd265a360edd521e5576ad33584a5
Gpayton787 <107384112+Gpayton787@users.noreply.github.com>
2024-01-31 15:42:45 -0800

    Finished maybe

--- pipe.c
+++ pipe.c
@@ -46,46 +46,8 @@ int main(int argc, char *argv[])
 		}	
 	}
 	//Run the remaining program
-	execlp(argv[0]);
-
+	execlp(argv[argc-1], argv[argc-1], NULL);
 
 	return 0;
 
-	//Static 2 argument program
-
-	int fds[2];
-	pipe(fds); //Create a pipe
-
-	int return_code = fork();
-	if(return_code == 0){
-		//Child
-		printf("Child Process: \n");
-		printf("Redirect stdout and run program A\n");
-		dup2(fds[1], STDOUT_FILENO);
-		close(fds[1]);
-		close(fds[0]);
-		execlp("ls", "ls", NULL);
-		//break here
-		printf("SHOULD NOT BE PRINTED");
-		exit(0);
-	}
-	else if(return_code > 0){
-		printf("Parent Process: \n");
-		int pid = return_code;
-		int status = 0;
-		waitpid(pid, &status, 0);
-		printf("Child process done, exited with code: %d\n", WEXITSTATUS(status));
-		printf("Redirct stdin run program B\n");
-		dup2(fds[0], STDIN_FILENO);
-		close(fds[0]);
-		close(fds[1]);
-		execlp("cat", "cat", NULL);
-		printf("SHOULD NOT BE PRINTED");
-		
-	}
-	else{
-		printf("Child process creation error\n");
-	}
-	
-	return 0;
 }
